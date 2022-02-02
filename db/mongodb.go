1182fac4cf2118d414c219eaa7c7e26ab3baf809
Kirill Lomoy <lesnov1999@gmail.com>
2022-02-02 11:15:19 +0300

    added new db requests

--- db/mongodb.go
+++ db/mongodb.go
@@ -6,8 +6,9 @@ import (
 	"strings"
 	"time"
 
-	db "github.com/HomelessHunter/CTC/db/models"
+	models "github.com/HomelessHunter/CTC/db/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -16,7 +17,7 @@ func GetUserCollection(client *mongo.Client) *mongo.Collection {
 	return client.Database("crypto_bot").Collection("users", options.Collection())
 }
 
-func InsertNewUser(coll *mongo.Collection, user *db.MongoUser, ctx context.Context) error {
+func InsertNewUser(coll *mongo.Collection, user *models.MongoUser, ctx context.Context) error {
 	_, err := coll.InsertOne(ctx, user)
 	if err != nil {
 		return err
@@ -24,9 +25,9 @@ func InsertNewUser(coll *mongo.Collection, user *db.MongoUser, ctx context.Conte
 	return nil
 }
 
-func GetUserByID(coll *mongo.Collection, id int64, ctx context.Context) (*db.MongoUser, error) {
-	var user db.MongoUser
-	err := coll.FindOne(ctx, bson.D{{"_id", id}}).Decode(&user)
+func GetUserByID(coll *mongo.Collection, id int64, ctx context.Context) (*models.MongoUser, error) {
+	var user models.MongoUser
+	err := coll.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}}).Decode(&user)
 	if err != nil {
 		return nil, err
 	}
@@ -35,15 +36,17 @@ func GetUserByID(coll *mongo.Collection, id int64, ctx context.Context) (*db.Mon
 }
 
 func DeleteUserByID(coll *mongo.Collection, id int64, ctx context.Context) error {
-	_, err := coll.DeleteOne(ctx, bson.D{{"_id", id}})
+	_, err := coll.DeleteOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}})
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func AddAlert(coll *mongo.Collection, id int64, alert *db.Alert, ctx context.Context) error {
-	_, err := coll.UpdateByID(ctx, id, bson.D{{"$push", bson.D{{"alerts", alert}}}, {"$set", bson.D{{"timestamp", time.Now()}}}})
+func AddAlert(coll *mongo.Collection, id int64, alert *models.Alert, ctx context.Context) error {
+	_, err := coll.UpdateByID(ctx, id, bson.D{
+		primitive.E{Key: "$push", Value: bson.D{primitive.E{Key: "alerts", Value: alert}}},
+		primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "timestamp", Value: time.Now().In(time.UTC)}}}})
 	if err != nil {
 		return err
 	}
@@ -51,13 +54,109 @@ func AddAlert(coll *mongo.Collection, id int64, alert *db.Alert, ctx context.Con
 	return nil
 }
 
-func GetAlertsPairs(coll *mongo.Collection, id int64, ctx context.Context) ([]string, error) {
-	result, err := coll.Distinct(ctx, "alerts.pair", bson.D{{"_id", id}})
+func RemoveAlert(coll *mongo.Collection, id int64, pair string, ctx context.Context) error {
+	_, err := coll.UpdateByID(ctx, id, bson.D{primitive.E{Key: "$pull", Value: bson.D{primitive.E{Key: "alerts", Value: bson.D{primitive.E{Key: "pair", Value: pair}}}}},
+		primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "timestamp", Value: time.Now().In(time.UTC)}}}})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Random order
+func GetPairs(coll *mongo.Collection, id int64, ctx context.Context) ([]string, error) {
+	result, err := coll.Distinct(ctx, "alerts.pair", bson.D{primitive.E{Key: "_id", Value: id}})
 	if err != nil {
 		return nil, err
 	}
+
+	return splitPairs(result), nil
+}
+
+func GetPairsByMarket(coll *mongo.Collection, id int64, market string, ctx context.Context) ([]string, error) {
+	var user models.MongoUser
+	err := coll.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}},
+		options.FindOne().SetProjection(bson.D{primitive.E{Key: "_id", Value: 0},
+			primitive.E{Key: "chat_id", Value: 0}, primitive.E{Key: "timestamp", Value: 0}})).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	pairs := make([]string, len(user.Alerts))
+
+	index := 0
+	for _, v := range user.Alerts {
+		if v.Market == market {
+			pairs[index] = v.Pair
+			index++
+		}
+	}
+
+	pairs = pairs[:index]
+
+	return pairs, nil
+}
+
+// func removePair(pairs []string, index int) []string {
+// 	return append(pairs[:index], pairs[index+1:]...)
+// }
+
+func splitPairs(result []interface{}) []string {
 	pairs := fmt.Sprint(result)
 	pairs = pairs[1 : len(pairs)-1]
+	return strings.Split(pairs, " ")
+}
 
-	return strings.Split(pairs, " "), nil
+func MarketExist(coll *mongo.Collection, id int64, market string, ctx context.Context) (bool, error) {
+	result := coll.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}, primitive.E{Key: "alerts.market", Value: market}})
+	if err := result.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func getChangeStream(coll *mongo.Collection, ctx context.Context) (*mongo.ChangeStream, error) {
+	pipeline := mongo.Pipeline{bson.D{primitive.E{Key: "$match", Value: bson.D{primitive.E{Key: "operationType", Value: "update"}}}}}
+	cs, err := coll.Watch(ctx, pipeline, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+	if err != nil {
+		return nil, err
+	}
+	return cs, nil
+}
+
+func WatchForChanges(coll *mongo.Collection, chngChan chan<- models.MongoUser, ctx context.Context) error {
+	cs, err := getChangeStream(coll, ctx)
+	if err != nil {
+		return err
+	}
+	defer cs.Close(ctx)
+
+	for cs.Next(ctx) {
+		var event bson.M
+		user, err := models.NewMongoUser()
+		if err != nil {
+			return err
+		}
+		err = cs.Decode(&event)
+		if err != nil {
+			if err == mongo.ErrClientDisconnected {
+				return nil
+			}
+			return fmt.Errorf("cannot decode event: %s", err)
+		}
+		output, err := bson.Marshal(event["fullDocument"])
+		if err != nil {
+			return err
+		}
+
+		err = bson.Unmarshal(output, user)
+		if err != nil {
+			return err
+		}
+		chngChan <- *user
+	}
+	return nil
 }
--- db/mongodb_test.go
+++ db/mongodb_test.go
@@ -12,60 +12,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func TestUserDB(t *testing.T) {
+const ID int64 = 1
+
+func TestInsertUser(t *testing.T) {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
 	if err != nil {
 		t.Errorf("Cannot connect %s", err)
 	}
 	defer client.Disconnect(context.TODO())
 	coll := GetUserCollection(client)
-	alert, err := db.NewAlert(db.WithPair("btcbusd"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
-	if err != nil {
-		t.Errorf("Cannot create alert %s", err)
-	}
-	user, err := db.NewMongoUser(db.WithUserID(1), db.WithAlerts(*alert))
-	if err != nil {
-		t.Errorf("Cannot create user %s", err)
-	}
-	err = InsertNewUser(coll, user, context.TODO())
-	if err != nil {
-		t.Errorf("Cannot insert user %s", err)
-	}
-	user, err = GetUserByID(coll, 1, context.TODO())
-	if err != nil {
-		t.Errorf("Cannot get a user %s", err)
-	}
-	fmt.Println(*user)
 
-	<-time.After(3 * time.Second)
-
-	alert, err = db.NewAlert(db.WithPair("ethbusd"), db.WithTargetPrice(4000.0))
-	if err != nil {
-		t.Errorf("Cannot create alert %s", err)
-	}
-	err = AddAlert(coll, 1, alert, context.TODO())
-	if err != nil {
-		t.Errorf("Cannot add new alert %s", err)
-	}
-	user, err = GetUserByID(coll, 1, context.TODO())
-	if err != nil {
-		t.Errorf("Cannot get a user %s", err)
-	}
-	fmt.Println(*user)
-
-	err = DeleteUserByID(coll, 1, context.TODO())
-	if err != nil {
-		t.Errorf("Cannot delete user %s", err)
-	}
-}
-
-func TestDisctinct(t *testing.T) {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
-	if err != nil {
-		t.Errorf("Cannot connect %s", err)
-	}
-	defer client.Disconnect(context.TODO())
-	coll := GetUserCollection(client)
 	alertBi, err := db.NewAlert(db.WithPair("btcbusd"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
 	if err != nil {
 		t.Errorf("Cannot create alert %s", err)
@@ -74,7 +30,92 @@ func TestDisctinct(t *testing.T) {
 	if err != nil {
 		t.Errorf("Cannot create alert %s", err)
 	}
-	user, err := db.NewMongoUser(db.WithUserID(1), db.WithAlerts(*alertBi, *alertHu))
+	user, err := db.NewMongoUser(db.WithUserID(ID), db.WithAlerts(*alertBi, *alertHu))
+	if err != nil {
+		t.Errorf("Cannot create user %s", err)
+	}
+	err = InsertNewUser(coll, user, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot insert user %s", err)
+	}
+}
+
+func TestGetUserByID(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer client.Disconnect(context.TODO())
+	coll := GetUserCollection(client)
+
+	user, err := GetUserByID(coll, ID, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot get a user %s", err)
+	}
+	fmt.Println(*user)
+}
+
+func TestDeleteUserByID(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer client.Disconnect(context.TODO())
+	coll := GetUserCollection(client)
+
+	err = DeleteUserByID(coll, ID, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot delete user %s", err)
+	}
+}
+
+func TestAddAlert(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer client.Disconnect(context.TODO())
+	coll := GetUserCollection(client)
+
+	alert, err := db.NewAlert(db.WithPair("ethbusd"), db.WithTargetPrice(4000.0))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+	err = AddAlert(coll, ID, alert, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot add new alert %s", err)
+	}
+}
+
+func TestDisctinct(t *testing.T) {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer client.Disconnect(context.TODO())
+	coll := GetUserCollection(client)
+
+	result, err := GetPairs(coll, ID, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot get pairs %s", err)
+	}
+	fmt.Println(result)
+}
+
+func TestRemoveAlert(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer client.Disconnect(context.TODO())
+	coll := GetUserCollection(client)
+
+	alertHu, err := db.NewAlert(db.WithPair("btcusdt"), db.WithTargetPrice(54000.0), db.WithMarket("huobi"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+	user, err := db.NewMongoUser(db.WithUserID(ID), db.WithAlerts(*alertHu))
 	if err != nil {
 		t.Errorf("Cannot create user %s", err)
 	}
@@ -83,14 +124,190 @@ func TestDisctinct(t *testing.T) {
 		t.Errorf("Cannot insert user %s", err)
 	}
 
-	result, err := GetAlertsPairs(coll, 1, context.TODO())
+	err = RemoveAlert(coll, ID, "btcusdt", context.TODO())
 	if err != nil {
-		t.Errorf("Cannot get pairs %s", err)
+		t.Errorf("Cannot remove alert %s", err)
 	}
-	fmt.Println(result)
 
-	err = DeleteUserByID(coll, 1, context.TODO())
+	user, err = GetUserByID(coll, ID, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot get a user %s", err)
+	}
+
+	if len(user.Alerts) > 0 {
+		t.Errorf("lenght of alerts should be 0 but %d instead", len(user.Alerts))
+	}
+
+	fmt.Println(user)
+
+	err = DeleteUserByID(coll, ID, context.TODO())
 	if err != nil {
 		t.Errorf("Cannot delete user %s", err)
 	}
+}
+
+func TestMarketExist(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer client.Disconnect(context.TODO())
+	coll := GetUserCollection(client)
+
+	insertUser := func(id int64, market string) {
+		alertBi, err := db.NewAlert(db.WithPair("ethusdt"), db.WithTargetPrice(54000.0), db.WithMarket(market))
+		if err != nil {
+			t.Errorf("Cannot create alert %s", err)
+		}
+		alertHu, err := db.NewAlert(db.WithPair("btcusdt"), db.WithTargetPrice(54000.0), db.WithMarket(market))
+		if err != nil {
+			t.Errorf("Cannot create alert %s", err)
+		}
+		user, err := db.NewMongoUser(db.WithUserID(id), db.WithAlerts(*alertBi, *alertHu))
+		if err != nil {
+			t.Errorf("Cannot create user %s", err)
+		}
+		err = InsertNewUser(coll, user, context.TODO())
+		if err != nil {
+			t.Errorf("Cannot insert user %s", err)
+		}
+	}
+
+	insertUser(ID, "huobi")
+	insertUser(2, "binance")
+
+	exist, err := MarketExist(coll, ID, "binance", context.TODO())
+	if err != nil {
+		t.Errorf("Cannot check if market exist: %s", err)
+	}
+	fmt.Println(exist)
+
+	err = DeleteUserByID(coll, ID, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot delete user %s", err)
+	}
+	err = DeleteUserByID(coll, 2, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot delete user %s", err)
+	}
+}
+
+func TestGetPairsByMarket(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+
+	coll := GetUserCollection(client)
+	defer func() {
+		err = DeleteUserByID(coll, ID, context.TODO())
+		if err != nil {
+			t.Errorf("Cannot delete user %s", err)
+		}
+		client.Disconnect(context.TODO())
+	}()
+
+	alertBi, err := db.NewAlert(db.WithPair("ethbusd"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+	alertBin, err := db.NewAlert(db.WithPair("btcusdt"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+
+	alertHuo, err := db.NewAlert(db.WithPair("ethusdt"), db.WithTargetPrice(54000.0), db.WithMarket("huobi"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+
+	alertBina, err := db.NewAlert(db.WithPair("solusdt"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+
+	user, err := db.NewMongoUser(db.WithUserID(ID), db.WithAlerts(*alertBi, *alertBin, *alertHuo, *alertBina), db.WithChatID(1))
+	if err != nil {
+		t.Errorf("Cannot create user %s", err)
+	}
+	err = InsertNewUser(coll, user, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot insert user %s", err)
+	}
+
+	pairs, err := GetPairsByMarket(coll, ID, "huobi", context.TODO())
+	if err != nil {
+		t.Errorf("Cannot get pairs: %s", err)
+	}
+	fmt.Println(pairs)
+}
+
+func TestWatchForChanges(t *testing.T) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		t.Errorf("Cannot connect %s", err)
+	}
+	defer func() {
+		TestDeleteUserByID(t)
+		client.Disconnect(context.TODO())
+	}()
+	coll := GetUserCollection(client)
+
+	alertBi, err := db.NewAlert(db.WithPair("ethbusd"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+	alertBin, err := db.NewAlert(db.WithPair("btcusdt"), db.WithTargetPrice(54000.3), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+
+	alertHuo, err := db.NewAlert(db.WithPair("ethusdt"), db.WithTargetPrice(54000.0), db.WithMarket("huobi"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+
+	alertBina, err := db.NewAlert(db.WithPair("solusdt"), db.WithTargetPrice(54000.0), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+
+	user, err := db.NewMongoUser(db.WithUserID(ID), db.WithAlerts(*alertBi, *alertBin, *alertHuo, *alertBina), db.WithChatID(1))
+	if err != nil {
+		t.Errorf("Cannot create user %s", err)
+	}
+	err = InsertNewUser(coll, user, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot insert user %s", err)
+	}
+
+	chngChan := make(chan db.MongoUser, 2)
+
+	go func() {
+		err = WatchForChanges(coll, chngChan, context.TODO())
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	<-time.After(2 * time.Second)
+
+	err = RemoveAlert(coll, ID, "ethusdt", context.TODO())
+	if err != nil {
+		t.Error(err)
+	}
+	// fmt.Println(<-chngChan)
+
+	<-time.After(2 * time.Second)
+
+	alert, err := db.NewAlert(db.WithPair("bnbbusd"), db.WithTargetPrice(400.0), db.WithMarket("binance"))
+	if err != nil {
+		t.Errorf("Cannot create alert %s", err)
+	}
+	err = AddAlert(coll, ID, alert, context.TODO())
+	if err != nil {
+		t.Errorf("Cannot add new alert %s", err)
+	}
+	f, s := <-chngChan, <-chngChan
+	fmt.Println(f, s)
 }
