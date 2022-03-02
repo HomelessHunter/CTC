ff2bab3142568f586143f28858c30dcb3dcc31b7
Kirill Lomoy <lesnov1999@gmail.com>
2022-03-02 09:57:17 +0300

    Changed find logic

--- cmd/utils.go
+++ cmd/utils.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
-	"sort"
+
+	db "github.com/HomelessHunter/CTC/db/models"
 )
 
-func pairExist(pair string, pairs []string) bool {
-	if i := pairSearch(pairs, pair); i < len(pairs) && i >= 0 {
-		if pairs[i] == pair {
-			return true
-		}
-	}
+// func pairExist(pair string, pairs []string) bool {
+// 	if i := pairSearch(pairs, pair); i < len(pairs) && i >= 0 {
+// 		if pairs[i] == pair {
+// 			return true
+// 		}
+// 	}
 
-	return false
+// 	return false
+// }
+
+func pairExist(market, pair string, alerts []db.Alert) bool {
+	if len(alerts) == 0 {
+		return false
+	}
+	alert := db.Alert{Market: market, Pair: pair}
+	_, err := alert.Find(alerts)
+	if err != nil {
+		fmt.Printf("cannot find alert: %s", err)
+		return false
+	}
+	return true
 }
 
 func compileRegexp() map[string]*regexp.Regexp {
@@ -25,24 +40,25 @@ func compileRegexp() map[string]*regexp.Regexp {
 	}
 }
 
-func checkDisconnectMsg(pair string, pairs []string) (int, error) {
-	if len(pairs) == 0 {
-		return -1, errors.New("pairs shouldn't be empty")
+func findDisconnectAlert(market, pair string, alerts []db.Alert) (int, error) {
+	if len(alerts) == 0 {
+		return -2, errors.New("pairs shouldn't be empty")
 	}
 	switch pair {
 	case "all":
 		return -1, nil
 	default:
-		return pairSearch(pairs, pair), nil
+		alert := db.Alert{Market: market, Pair: pair}
+		return alert.Find(alerts)
 	}
 }
 
-func pairSearch(pairs []string, pair string) int {
-	p := sort.StringSlice(pairs)
-	p.Sort()
-	return p.Search(pair)
-}
+// func pairSearch(pairs []string, pair string) int {
+// 	p := sort.StringSlice(pairs)
+// 	p.Sort()
+// 	return p.Search(pair)
+// }
 
-func removePair(pairs []string, index int) []string {
-	return append(pairs[:index], pairs[index+1:]...)
-}
+// func removePair(pairs []string, index int) []string {
+// 	return append(pairs[:index], pairs[index+1:]...)
+// }
